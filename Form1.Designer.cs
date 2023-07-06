d6fc0bc222df276a1e7ada2cc63bdb3172677119
Fairydaa <firdaaas123@gmail.com>
2023-07-06 21:44:03 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace hospital_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            button5 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            groupBox2 = new GroupBox();
+            button4 = new Button();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Location = new Point(23, 16);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(754, 120);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Master data";
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(255, 128, 128);
+            button5.Location = new Point(730, 0);
+            button5.Name = "button5";
+            button5.Size = new Size(24, 23);
+            button5.TabIndex = 3;
+            button5.Text = "X";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(314, 38);
+            button3.Name = "button3";
+            button3.Size = new Size(100, 23);
+            button3.TabIndex = 2;
+            button3.Text = "Data Obat";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(173, 38);
+            button2.Name = "button2";
+            button2.Size = new Size(100, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Data Dokter";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(31, 38);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Data Pasien";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button4);
+            groupBox2.Location = new Point(23, 188);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(750, 126);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Data Pemerikasaan/CheckUp";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(31, 33);
+            button4.Name = "button4";
+            button4.Size = new Size(100, 23);
+            button4.TabIndex = 0;
+            button4.Text = "Data CheckUp";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(23, 317);
+            label1.Name = "label1";
+            label1.Size = new Size(392, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Dikembangkan Oleh: D3 Teknik Informatika - Politeknik Negeri Semarang";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(817, 350);
+            ControlBox = false;
+            Controls.Add(label1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Rumah Sakit Mitra Sehat";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button button5;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private GroupBox groupBox2;
+        private Button button4;
+        private Label label1;
+    }
+}
---
+++ Form5.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace hospital_1
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button6 = new Button();
+            button3 = new Button();
+            textBox5 = new TextBox();
+            textBox3 = new TextBox();
+            comboBox1 = new ComboBox();
+            dateTimePicker1 = new DateTimePicker();
+            label7 = new Label();
+            dataGridView1 = new DataGridView();
+            button5 = new Button();
+            button4 = new Button();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            button2 = new Button();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            textBox6 = new TextBox();
+            label8 = new Label();
+            textBox7 = new TextBox();
+            label9 = new Label();
+            textBox8 = new TextBox();
+            comboBox2 = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button6
+            // 
+            button6.Location = new Point(504, 338);
+            button6.Margin = new Padding(3, 2, 3, 2);
+            button6.Name = "button6";
+            button6.Size = new Size(82, 22);
+            button6.TabIndex = 52;
+            button6.Text = "Cari";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(745, 338);
+            button3.Margin = new Padding(3, 2, 3, 2);
+            button3.Name = "button3";
+            button3.Size = new Size(82, 22);
+            button3.TabIndex = 51;
+            button3.Text = "Edit";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            textBox5.ForeColor = SystemColors.ButtonShadow;
+            textBox5.Location = new Point(347, 338);
+            textBox5.Margin = new Padding(3, 2, 3, 2);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(152, 23);
+            textBox5.TabIndex = 50;
+            textBox5.Text = "Cari Data Dokter";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(209, 31);
+            textBox3.Margin = new Padding(3, 2, 3, 2);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(119, 23);
+            textBox3.TabIndex = 49;
+            textBox3.Visible = false;
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(114, 85);
+            comboBox1.Margin = new Padding(3, 2, 3, 2);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(216, 23);
+            comboBox1.TabIndex = 48;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(113, 142);
+            dateTimePicker1.Margin = new Padding(3, 2, 3, 2);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(216, 23);
+            dateTimePicker1.TabIndex = 45;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(579, 67);
+            label7.Name = "label7";
+            label7.Size = new Size(107, 15);
+            label7.TabIndex = 44;
+            label7.Text = "Table Daftar Dokter";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(347, 85);
+            dataGridView1.MultiSelect = false;
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(561, 246);
+            dataGridView1.TabIndex = 43;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(112, 308);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 42;
+            button5.Text = "Reset";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(833, 338);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 41;
+            button4.Text = "Hapus";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 204);
+            label5.Name = "label5";
+            label5.Size = new Size(73, 15);
+            label5.TabIndex = 39;
+            label5.Text = "Penanganan";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 175);
+            label4.Name = "label4";
+            label4.Size = new Size(56, 15);
+            label4.TabIndex = 38;
+            label4.Text = "Diagnosa";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 146);
+            label3.Name = "label3";
+            label3.Size = new Size(99, 15);
+            label3.TabIndex = 37;
+            label3.Text = "Tanggal CheckUp";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 117);
+            label2.Name = "label2";
+            label2.Size = new Size(76, 15);
+            label2.TabIndex = 36;
+            label2.Text = "Nama Pasien";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 88);
+            label1.Name = "label1";
+            label1.Size = new Size(77, 15);
+            label1.TabIndex = 35;
+            label1.Text = "Nama Dokter";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(209, 308);
+            button2.Name = "button2";
+            button2.Size = new Size(119, 23);
+            button2.TabIndex = 34;
+            button2.Text = "Simpan";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(113, 172);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(216, 23);
+            textBox4.TabIndex = 33;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 28);
+            button1.Name = "button1";
+            button1.Size = new Size(123, 23);
+            button1.TabIndex = 30;
+            button1.Text = "<< Kembali";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(113, 201);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(216, 23);
+            textBox6.TabIndex = 53;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(12, 233);
+            label8.Name = "label8";
+            label8.Size = new Size(35, 15);
+            label8.TabIndex = 55;
+            label8.Text = "Biaya";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(113, 233);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(217, 23);
+            textBox7.TabIndex = 54;
+            textBox7.TextChanged += textBox7_TextChanged;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(11, 272);
+            label9.Name = "label9";
+            label9.Size = new Size(63, 15);
+            label9.TabIndex = 57;
+            label9.Text = "Total Biaya";
+            label9.Click += label9_Click;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(113, 269);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(217, 23);
+            textBox8.TabIndex = 56;
+            // 
+            // comboBox2
+            // 
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(114, 114);
+            comboBox2.Margin = new Padding(3, 2, 3, 2);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(216, 23);
+            comboBox2.TabIndex = 58;
+            // 
+            // Form5
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(885, 450);
+            Controls.Add(comboBox2);
+            Controls.Add(label9);
+            Controls.Add(textBox8);
+            Controls.Add(label8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(button6);
+            Controls.Add(button3);
+            Controls.Add(textBox5);
+            Controls.Add(textBox3);
+            Controls.Add(comboBox1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label7);
+            Controls.Add(dataGridView1);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(textBox4);
+            Controls.Add(button1);
+            Name = "Form5";
+            Text = "Data CheckUp";
+            Load += Form5_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button6;
+        private Button button3;
+        private TextBox textBox5;
+        private TextBox textBox3;
+        private ComboBox comboBox1;
+        private DateTimePicker dateTimePicker1;
+        private Label label7;
+        private DataGridView dataGridView1;
+        private Button button5;
+        private Button button4;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Button button2;
+        private TextBox textBox4;
+        private Button button1;
+        private TextBox textBox6;
+        private Label label8;
+        private TextBox textBox7;
+        private Label label9;
+        private TextBox textBox8;
+        private ComboBox comboBox2;
+    }
+}
